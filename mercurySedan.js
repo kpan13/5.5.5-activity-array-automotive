681ced325176c0a5ef9c84b8e29ac024f1ccbc39
kpan13 <158117798+kpan13@users.noreply.github.com>
2024-03-26 21:23:50 -0700

    Update mercurySedan.js

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,35 +1,45 @@
-//this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+        
+    }
+
+    checkService(){
+        if (this.mileage > 30000) {
+            this.scheduleService = true
+            return this.schedulesService;
+        }
+
+
+    }
+
+    loadPassenger(num){
+        if(this.passenger < this.maxPassengers) {
+            if ((num +  this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passemger;
+            } else{
+                console.log(this.model + " " + this.make + "no enough space for all passengers");
+            }
+        } else {
+            console.log(this.model +" " + this.make + "is full");
+        }
+    }
+
+
+ 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
-
-
-//After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
-
-
-
-
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
+myCar.start()
+myCar.loadPassenger()
+myCar.stop()
+myCar.checkService()
+console.log(myCar)
